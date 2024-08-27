21d4ed608e1821e33fb01217462fa1a5c9f89e1d
Aouab <aouabsss@gmail.com>
2024-08-27 14:15:12 +0200

    use pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'DiscordBot',
+        script: 'npm',
+        args: 'run start:production',
+        output: `/logs/${new Date().toISOString().slice(0, 10)}-out.log`,
+        error: `/logs/${new Date().toISOString().slice(0, 10)}-error.log`,
+      }
+    ]
+  };
+  
