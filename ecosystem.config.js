5bdc943c827bed334eee5cead5be2154f4dea3ba
Aouab <aouabsss@gmail.com>
2024-08-27 14:17:35 +0200

    fix ecosystem log path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
         name: 'DiscordBot',
         script: 'npm',
         args: 'run start:production',
-        output: `/logs/${new Date().toISOString().slice(0, 10)}-out.log`,
-        error: `/logs/${new Date().toISOString().slice(0, 10)}-error.log`,
+        output: `./logs/${new Date().toISOString().slice(0, 10)}-out.log`,
+        error: `./logs/${new Date().toISOString().slice(0, 10)}-error.log`,
       }
     ]
   };
